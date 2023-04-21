5530104631e43c166dd669b135ae9575af2254bb
Nurselam <77880259+WNurselam@users.noreply.github.com>
2023-04-21 21:25:25 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from "react-query";
 
